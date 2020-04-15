2b03fb4a3a3be402548f6dc63355102e402dae5f
Shengyang Wu <wsywu5354@gmail.com>
2020-04-15 11:53:26 -0700

    Add Images

--- SnakeAI/src/game/Test.java
+++ SnakeAI/src/game/Test.java
@@ -2,6 +2,10 @@ package game;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,15 +17,34 @@ public class Test extends JPanel{
     private static final long serialVersionUID = 1L;
     
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 5; j < 8; j++) {
-                System.out.println(j);
-                if (j > 6) {
-                    continue;
-                }
-            }
+        System.out.println(111);
+        final String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+        File currentJar = null;
+        try {
+            currentJar = new File(Test.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+
         }
+        
+        final ArrayList<String> command = new ArrayList<String>();
+        command.add(javaBin);
+        command.add("-jar");
+        command.add(currentJar.getPath());
+
+        final ProcessBuilder builder = new ProcessBuilder(command);
+        try {
+            builder.start();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.exit(0);
+     
     }
     
     
 }
+    
